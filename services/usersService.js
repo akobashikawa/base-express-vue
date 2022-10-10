cbf080d291880985f5b0474d5420abee24c248a7
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-10-10 02:16:57 -0500

    Implement users rest

--- services/usersService.js
+++ services/usersService.js
@@ -27,6 +27,34 @@ const usersService = {
         var params = [];
         return this.query(sql, params);
     },
+
+    findById(id) {
+        const sql = `SELECT * FROM Users WHERE id=${id}`;
+        const params = [];
+        return this.query(sql, params);
+    },
+
+    add(data) {
+        const username = data.username;
+        const publickey = data.publickey;
+        const sql = `INSERT INTO Users
+        (username, publickey)
+        VALUES('${username}', '${publickey}');
+        `;
+        const params = [];
+        return this.query(sql, params);
+    },
+
+    update(id, data) {
+        const username = data.username;
+        const publickey = data.publickey;
+        const sql = `UPDATE Users
+        SET username='${username}', publickey='${publickey}'
+        WHERE id=${id}
+        `;
+        const params = [];
+        return this.query(sql, params);
+    },
 };
 
 module.exports = usersService;
