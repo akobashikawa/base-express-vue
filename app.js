a11b5e582b2f0af3df1f52aecec62056805816a0
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-10-10 02:06:00 -0500

    Add cors
    Add cross-env

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+if (app.get('env') == 'development') {
+    console.log('development env');
+    app.use(cors());
+    console.log('Enabled All CORS Requests');
+}
 
 app.use(logger('dev'));
 app.use(express.json());
